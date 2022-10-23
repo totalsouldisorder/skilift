b1ba765acf144b89043f5554f3be015abf50f43b
gucskifer <116482803+gucskifer@users.noreply.github.com>
2022-10-23 16:35:13 -0500

    add comments

--- skilift.sh
+++ skilift.sh
@@ -21,64 +21,63 @@ makepkg -si
 
 PKGS=(
 		# XORG
-        'xorg-server'           	
-        'xorg-apps'             	
-        'xorg-xinit'            	
-        'xf86-video-intel'      	
-        'mesa'                  	
+        'xorg-server'           	   # Xorg X server
+        'xorg-apps'             	   # Group of Xorg apps
+        'xorg-xinit'            	   # Xorg initialisation program
+        'xf86-video-intel'      	   # X.org Intel i810/i830/i915/945G/G965+ video drivers
+        'mesa'                  	   # An open-source implementation of the OpenGL specification
         # KDE PLASMA
-        'plasma'       				
-        'sddm'         				
-        'kdeplasma-addons'
-        'kdeconnect'
+        'plasma'       			   	   # Group for Plasma desktop and software
+        'sddm'         				   # QML based X11 and Wayland display manager
+        'kdeplasma-addons'             # All kind of addons to improve your Plasma experience
+        'kdeconnect'                   # Adds communication between KDE and your smartphone
         # NETWORKING
-        'wpa_supplicant'          
-        'dialog'                   
-        'networkmanager'            
-        'openvpn'                  
-        'networkmanager-openvpn'    
-        'network-manager-applet'    
-        'dhcp'                  	
-        'libsecret'                
+        'wpa_supplicant'               # A utility providing key negotiation for WPA wireless networks
+        'dialog'                       # A tool to display dialog boxes from shell scripts
+        'networkmanager'               # Network connection manager and user applications
+        'openvpn'                      # An easy-to-use, robust and highly configurable VPN (Virtual Private Network)
+        'networkmanager-openvpn'       # NetworkManager VPN plugin for OpenVPN
+        'network-manager-applet'       # Applet for managing network connections
+        'dhcp'                  	   # RFC2131 compliant DHCP client daemon
+        'libsecret'                    # Library for storing and retrieving passwords and other secrets
         # BLUETOOTH
-        'bluez'                 	
-        'bluez-utils'            	
-        'bluez-libs'
+        'bluez'                 	   # Daemons for the bluetooth protocol stack
+        'bluez-utils'            	   # Development and debugging utilities for the bluetooth protocol stack
+        'bluez-libs'                   # Deprecated libraries for the bluetooth protocol stack
         # AUDIO
-        'pipewire'
-        'pipewire-alsa'
-        'pipewire-jack'
-        'pipewire-pulse'
+        'pipewire'                     # Low-latency audio/video router and processor
+        'pipewire-alsa'                # Low-latency audio/video router and processor - ALSA configuration
+        'pipewire-jack'                # Low-latency audio/video router and processor - JACK support
+        'pipewire-pulse'               # Low-latency audio/video router and processor - PulseAudio replacement
         # VIRTUALIZATION
-        'virtualbox'
-        'virtualbox-host-modules-arch'
+        'virtualbox'                   # Powerful x86 virtualization for enterprise as well as home use
+        'virtualbox-host-modules-arch' # Virtualbox host kernel modules for Arch Kernel
         # RICE
-        'alacritty'
-        'latte-dock'
-        'neofetch'
+        'alacritty'                    # A cross-platform, GPU-accelerated terminal emulator
+        'latte-dock'                   # A dock based on Plasma Frameworks
+        'neofetch'                     # A CLI system information tool written in BASH that supports displaying images.
         # GOODIES
-        'arandr'
-        'firefox'
-        'inkscape'
-        'keepassxc'
-        'mpv'
-        'nano'
-        'nvidia'
-        'screenfetch'
-        'spectacle'
-        'transmission-qt'
-        'youtube-dl'
+        'arandr'                       # Provide a simple visual front end for XRandR 1.2.
+        'firefox'                      # Standalone web browser from mozilla.org
+        'inkscape'                     # Professional vector graphics editor
+        'keepassxc'                    # Cross-platform community-driven port of Keepass password manager
+        'mpv'                          # a free, open source, and cross-platform media player
+        'nano'                         # Pico editor clone with enhancements
+        'nvidia'                       # NVIDIA drivers for linux
+        'spectacle'                    # KDE screenshot capture utility
+        'transmission-qt'              # Fast, easy, and free BitTorrent client (Qt GUI)
+        'youtube-dl'                   # A command-line program to download videos from YouTube.com and a few more sites
         # AUR PACKAGES
-        'apple-fonts'
-        'betterdiscord-installer'
-        'catppuccin-gtk-theme-frappe'
-        'catppuccin-gtk-theme-latte'
-        'catppuccin-gtk-theme-macchiato'
-        'catppuccin-gtk-theme-mocha'
-        'discord-canary-electron-bin'
-        'plasma5-applets-window-appmenu-git'
-        'protonvpn' 
-        'tela-icon-theme'
+        'apple-fonts'                           # Apple San Francisco, New York fonts, directly from Apple official source
+        'betterdiscord-installer'               # Installer for BetterDiscord
+        'catppuccin-gtk-theme-frappe'           # Soothing pastel theme for GTK3 - Frappe
+        'catppuccin-gtk-theme-latte'            # Soothing pastel theme for GTK3 - Latte
+        'catppuccin-gtk-theme-macchiato'        # Soothing pastel theme for GTK3 - Macchiato
+        'catppuccin-gtk-theme-mocha'            # Soothing pastel theme for GTK3 - Mocha
+        'discord-canary-electron-bin'           # Discord Canary (popular voice + video app) using the system provided electron for increased security and performance
+        'plasma5-applets-window-appmenu-git'    # Plasma 5 applet in order to show the window appmenu
+        'protonvpn'                             # Official ProtonVPN metapackage that installs protonvpn-gui and protonvpn-cli, maintained by the ProtonVPN team.
+        'tela-icon-theme'                       # A flat colorful design icon theme.
 )
 
 for PKG in "${PKGS[@]}"; do
